3a50f181a4263f19b454d8571a92e92c9f89dc7f
Matveev Stepan Andreevich <mset321@gmail.com>
2024-06-14 00:24:13 +0300

    работаю над заказами

--- controller.py
+++ controller.py
@@ -1,5 +1,8 @@
 from PyQt6.QtCore import QObject
 from PyQt6.QtWidgets import QTableWidgetItem
+
+from datetime import datetime
+
 from model import Model
 from view import View
 
@@ -10,70 +13,265 @@ class Controller(QObject):
         self.model = Model()
         self.view = View()
 
-        self.curClient = -1
-        self.orderClient = -1
-        self.clients = []
+        self.cur_client_ID = -1
+        self.cur_order_client_ID = -1
+
+        self.cur_order_id = -1
+        self.cur_menu_item_to_order_id = -1
+        self.cur_menu_item_from_order_id = -1
+
+        self.cur_status_id = 1
+        self.cur_employee_id = 1
+        self.cur_client_for_order_id = -1
+
+        self.clients = self.model.get_clients()
+        self.orders = self.model.get_orders()
+        if len(self.clients):
+            self.cur_client_ID = self.clients[-1][4]
+        if len(self.orders):
+            self.cur_order_id = self.orders[-1][5]
+
+        self.menu_items = self.model.get_menu_items()
+        self.statuses = self.model.get_order_statuses()
+        self.employees = self.model.get_employees()
 
         self.view.form.stackedWidget.setCurrentIndex(0)
 
         self.view.form.page_name_CB.currentIndexChanged.connect(self.page_name_CB_index_change)
 
-        self.view.form.clients_table.currentItemChanged.connect(self.set_cur_client)
+        self.view.form.clients_table.currentItemChanged.connect(self.cur_client_index_change)
         self.view.form.add_client_button.clicked.connect(self.add_client)
         self.view.form.change_client_button.clicked.connect(self.edit_client)
+        self.view.form.delete_client_button.clicked.connect(self.delete_client)
 
-        self.clients = self.model.get_clients()
-        self.curClient = len(self.clients) - 1
-        self.fill_clients_table()
+        self.view.form.orders_table.currentItemChanged.connect(self.cur_order_index_change)
+        self.view.form.clients_table_for_order.currentItemChanged.connect(self.cur_client_for_order_index_change)
+        self.view.form.orders_statuse_table.currentItemChanged.connect(self.cur_orders_statuses_index_change)
+        self.view.form.employee_table_for_order.currentItemChanged.connect(self.cur_employee_index_change)
+        self.view.form.menu_items_table_to_order.currentItemChanged.connect(self.cur_menu_item_to_order_index_change)
+        self.view.form.menu_item_from_order_table.currentItemChanged.connect(self.cur_menu_item_from_order_index_change)
+        # buttons for orders
+        self.view.form.add_order_button.clicked.connect(self.add_order)
+        self.view.form.delete_order_button.clicked.connect(self.delete_order)
+        self.view.form.change_order_status_button.clicked.connect(self.edit_order)
+        self.view.form.add_menu_item_to_order_button.clicked.connect(self.add_menu_item_to_order)
+        self.view.form.del_menu_item_from_order_button.clicked.connect(self.delete_menu_item_from_order)
+
+        self.fill_clients_table(self.view.form.clients_table)
 
     def page_name_CB_index_change(self, new_index: int):
         self.view.form.stackedWidget.setCurrentIndex(new_index)
-        self.fill_clients_table()
+        self.fill_clients_table(self.view.form.clients_table)
+        self.fill_clients_table(self.view.form.clients_table_for_order)
+        self.fill_orders_table()
+        self.fill_employees_table()
+        self.fill_menu_items_to_order_table()
+        self.fill_statuses_table()
 
-    def fill_clients_table(self):
-        self.view.form.clients_table.setRowCount(len(self.clients))
+        if len(self.clients):
+            self.cur_order_client_ID = self.cur_client_ID = self.clients[-1][4]
+
+    def fill_clients_table(self, client_table):
+        client_table.setRowCount(len(self.clients))
         i = 0
         for rec in self.clients:
-            self.view.form.clients_table.setItem(
+            client_table.setItem(
                 i, 0, QTableWidgetItem(str(rec[0]))
             )
-            self.view.form.clients_table.setItem(
+            client_table.setItem(
                 i, 1, QTableWidgetItem(str(rec[1]))
             )
-            self.view.form.clients_table.setItem(
+            client_table.setItem(
                 i, 2, QTableWidgetItem(str(rec[2]))
             )
-            self.view.form.clients_table.setItem(
+            client_table.setItem(
                 i, 3, QTableWidgetItem(str(rec[3]))
             )
             i += 1
 
-    def set_cur_client(self, current, previous):
-        new_cur = current.row()
-        self.curClient = new_cur
+    def cur_client_index_change(self, current, previous):
+        if current is not None:
+            new_cur = current.row()
+            self.set_cur_client(new_cur)
+
+    def set_cur_client(self, new_cur: int):
+        self.cur_client_ID = self.clients[new_cur][4]
         self.view.form.clients_full_name_LE.setText(str(self.clients[new_cur][0]))
         self.view.form.cliens_phone_LE.setText(str(self.clients[new_cur][1]))
 
     def add_client(self):
         full_name = self.view.form.clients_full_name_LE.text()
         phone_number = self.view.form.cliens_phone_LE.text()
+
         if len(phone_number) > 20:
-             return
-         
+            return
+
         if not self.model.is_exist_client(phone_number):
             self.model.insert_client(phone_number, full_name)
+            self.cur_client_ID = self.model.get_client_id_by_phone(phone_number)
+            self.model.insert_loyalty_program_account(0, self.cur_client_ID, 1)
+            self.model.update_client_lpa(self.cur_client_ID)
             self.clients = self.model.get_clients()
-            self.curClient = len(self.clients) - 1
-        self.fill_clients_table()
+            self.fill_clients_table(self.view.form.clients_table)
 
     def edit_client(self):
         full_name = self.view.form.clients_full_name_LE.text()
         phone_number = self.view.form.cliens_phone_LE.text()
+
         if len(phone_number) > 20:
             return
-        if not self.model.is_exist_client(phone_number):
-            pass
-        
+
+        cl_by_phone = self.model.get_client_id_by_phone(phone_number)
+        if cl_by_phone == self.cur_client_ID or cl_by_phone == -1:
+            self.model.edit_client(self.cur_client_ID, phone_number, full_name)
+            self.clients = self.model.get_clients()
+            self.fill_clients_table(self.view.form.clients_table)
+
+    def delete_client(self):
+        if self.cur_client_ID > 0:
+            self.model.delete_client(self.cur_client_ID)
+            if len(self.clients) == 1:
+                self.cur_client_ID = -1
+            else:
+                cur_cl_ind = 0
+                for i in range(0, len(self.clients)):
+                    if self.clients[i][4] == self.cur_client_ID:
+                        cur_cl_ind = i
+                        break
+                self.cur_client_ID = self.clients[max(0, cur_cl_ind - 1)][4]
+                self.set_cur_client(cur_cl_ind)
+            self.clients = self.model.get_clients()
+            self.fill_clients_table(self.view.form.clients_table)
+
+    def fill_orders_table(self):
+        self.view.form.orders_table.setRowCount(len(self.orders))
+        i = 0
+        orders_table = self.view.form.orders_table
+        for rec in self.orders:
+            orders_table.setItem(
+                i, 0, QTableWidgetItem(str(rec[0]))
+            )
+            orders_table.setItem(
+                i, 1, QTableWidgetItem(str(rec[1]))
+            )
+            orders_table.setItem(
+                i, 2, QTableWidgetItem(str(rec[2]))
+            )
+            orders_table.setItem(
+                i, 3, QTableWidgetItem(str(rec[3]))
+            )
+            orders_table.setItem(
+                i, 4, QTableWidgetItem(str(rec[4]))
+            )
+            i += 1
+
+    def fill_employees_table(self):
+        self.view.form.employee_table_for_order.setRowCount(len(self.employees))
+        i = 0
+        employees_table = self.view.form.employee_table_for_order
+        for rec in self.employees:
+            employees_table.setItem(
+                i, 0, QTableWidgetItem(str(rec[0]))
+            )
+            employees_table.setItem(
+                i, 1, QTableWidgetItem(str(rec[1]))
+            )
+            i += 1
+
+    def fill_menu_items_to_order_table(self):
+        self.view.form.menu_items_table_to_order.setRowCount(len(self.menu_items))
+        i = 0
+        menu_items_table = self.view.form.menu_items_table_to_order
+        for rec in self.menu_items:
+            menu_items_table.setItem(
+                i, 0, QTableWidgetItem(str(rec[0]))
+            )
+            menu_items_table.setItem(
+                i, 1, QTableWidgetItem(str(rec[1]))
+            )
+            i += 1
+
+    # TODO: сделать заполение состава заказа
+    def fill_menu_items_from_order_table(self):
+        pass
+
+    def fill_statuses_table(self):
+        self.view.form.orders_statuse_table.setRowCount(len(self.statuses))
+        i = 0
+        orders_statuses_table = self.view.form.orders_statuse_table
+        for rec in self.statuses:
+            orders_statuses_table.setItem(
+                i, 0, QTableWidgetItem(str(rec[0]))
+            )
+            i += 1
+
+    def cur_order_index_change(self, current, previous):
+        if current is not None:
+            new_cur = current.row()
+            self.set_cur_order(new_cur)
+
+    def set_cur_order(self, new_cur: int):
+        self.cur_order_id = self.orders[new_cur][5]
+        self.fill_menu_items_from_order_table()
+
+    def cur_client_for_order_index_change(self, current, previous):
+        if current is not None:
+            self.cur_order_client_ID = self.clients[current.row()][4]
+
+    def cur_employee_index_change(self, current, previous):
+        if current is not None:
+            self.cur_employee_id = self.employees[current.row()][2]
+
+    def cur_menu_item_to_order_index_change(self, current, previous):
+        if current is not None:
+            self.cur_menu_item_to_order_id = self.menu_items[current.row()][2]
+
+    def cur_menu_item_from_order_index_change(self, current, previous):
+        if current is not None:
+            self.cur_menu_item_from_order_id = self.model.get_menu_items_from_order(self.cur_order_id)[current.row()][2]
+
+    def cur_orders_statuses_index_change(self, current, previous):
+        if current is not None:
+            self.cur_status_id = self.statuses[current.row()][1]
+
+    def add_order(self):
+        self.model.insert_order(
+            datetime.now().strftime("%Y-%m-%d"),
+            self.cur_order_client_ID,
+            self.cur_employee_id,
+            self.cur_status_id,
+            0
+        )
+        self.orders = self.model.get_orders()
+        self.set_cur_order(-1)
+        self.fill_orders_table()
+
+    def delete_order(self):
+        if self.cur_order_id > 0:
+            self.model.delete_order(self.cur_order_id)
+            if len(self.orders) == 1:
+                self.cur_order_id = -1
+            else:
+                cur_ord_ind = 0
+                for i in range(0, len(self.orders)):
+                    if self.orders[i][5] == self.cur_order_id:
+                        cur_ord_ind = i
+                        break
+                self.cur_order_id = self.orders[max(0, cur_ord_ind - 1)][5]
+                self.set_cur_order(cur_ord_ind)
+            self.orders = self.model.get_orders()
+            self.fill_orders_table()
+
+    def edit_order(self):
+        pass
+
+    def add_menu_item_to_order(self):
+        pass
+
+    def delete_menu_item_from_order(self):
+        pass
+
+
 if __name__ == "__main__":
     import sys
     from PyQt6.QtWidgets import QApplication
@@ -82,5 +280,4 @@ if __name__ == "__main__":
 
     controller = Controller()
     controller.view.show()
-
     sys.exit(app.exec())
--- model.py
+++ model.py
@@ -22,7 +22,8 @@ class Model:
                 cl."full_name", 
                 cl."phone_number",
                 lpa."money_spent",
-                lpl."name"
+                lpl."name",
+                cl."ID"
             FROM 
                 "Clients" cl
             LEFT JOIN "Loyalty_program_accounts" lpa
@@ -42,34 +43,72 @@ class Model:
             (phone_number, full_name)
         )
         self.conn.commit()
-        
+
     def edit_client(self, cl_ID: int, phone_number: str, full_name: str) -> None:
         self.cur.execute(
             'UPDATE "Clients" SET "phone_number" = %s, "full_name" = %s WHERE "ID" = %s',
-            (phone_number, )
+            (phone_number, full_name, cl_ID)
         )
+        self.conn.commit()
 
-    def update_client_lpa(self, client_ID: int, lpa_ID):
-        pass
+    def delete_client(self, cl_ID: int) -> None:
+        self.cur.execute(
+            'DELETE FROM "Clients" WHERE "ID" = %s',
+            (cl_ID,)
+        )
+        self.conn.commit()
+
+    def update_client_lpa(self, client_ID: int):
+        self.cur.execute(
+            """
+            WITH lpa_ID AS (
+                SELECT 
+                    "ID", "client_ID"
+                FROM 
+                    "Loyalty_program_accounts" 
+                WHERE
+                    "client_ID" = %s
+            )
+            UPDATE 
+                "Clients" cl
+            SET 
+                "prgr_ID" = lpa_ID."ID"
+            FROM 
+                lpa_ID
+            WHERE 
+                cl."ID" = lpa_ID."client_ID"
+            """,
+            (client_ID,)
+        )
+        self.conn.commit()
 
     def is_exist_client(self, phone_number: str) -> bool:
         self.cur.execute("""SELECT EXISTS (
                             SELECT 1
                             FROM "Clients"
                             WHERE "phone_number" = %s
-                        );""", (phone_number, ))
+                        );""", (phone_number,))
         res = self.cur.fetchall()[0][0]
         return res
 
     def insert_loyalty_program_account(self, money_spent: int, client_id: int, lvl_id: int) -> None:
-        query = sql.SQL("INSERT INTO Loyalty_program_accounts (money_spent, client_id, lvl_id) VALUES (%s, %s, %s)")
+        query = sql.SQL(
+            'INSERT INTO "Loyalty_program_accounts" ("money_spent", "client_ID", "lvl_ID") VALUES (%s, %s, %s)'
+        )
         self.cur.execute(query, (money_spent, client_id, lvl_id))
         self.conn.commit()
 
     def insert_order(self, date: str, client_id: int, employee_id: int, status_id: int, total: int) -> None:
         query = sql.SQL(
-            "INSERT INTO Orders (date, client_id, employee_id, status_id, total) VALUES (%s, %s, %s, %s, %s)")
+            'INSERT INTO "Orders" ("date", "client_ID", "employee_ID", "status_ID", "total") VALUES (%s, %s, %s, %s, %s)')
         self.cur.execute(query, (date, client_id, employee_id, status_id, total))
+        self.conn.commit()
+
+    def delete_order(self, order_id: int) -> None:
+        self.cur.execute(
+            'DELETE FROM "Orders" WHERE "ID" = %s',
+            (order_id,)
+        )
         self.conn.commit()
 
     def insert_order_line(self, order_id: int, menu_item_id: int, quantity: int) -> None:
@@ -87,6 +126,56 @@ class Model:
         """)
         return self.cur.fetchone()
 
+    def get_employees(self) -> list[Any]:
+        self.cur.execute("""
+                        SELECT 
+                            e."full_name",
+                            jt."name",
+                            e."ID"
+                        FROM
+                            "Employees" e
+                        LEFT JOIN "Job_titles" jt
+                            ON jt."ID" = e."job_titel_ID"
+                        ORDER BY 
+                        e."ID"
+        """)
+        return self.cur.fetchall()
+
+    def get_order_statuses(self) -> list[Any]:
+        self.cur.execute(
+            'SELECT "name", "ID" FROM "Order_statuses" ORDER BY "ID"'
+        )
+        return self.cur.fetchall()
+
+    def get_menu_items(self) -> list[Any]:
+        self.cur.execute(
+            'SELECT "name", "price", "ID" FROM "Menu_items"'
+        )
+        return self.cur.fetchall()
+
+    def get_orders(self) -> list[Any]:
+        self.cur.execute(
+            """
+            SELECT 
+                o."date",
+                cl."full_name",
+                o."total",
+                os."name",
+                e."full_name",
+                o."ID"
+            FROM 
+                "Orders" o
+            LEFT JOIN "Clients" cl
+                ON cl."ID" = o."client_ID"
+            LEFT JOIN "Order_statuses" os
+                ON os."ID" = o."status_ID"
+            LEFT JOIN "Employees" e
+                ON e."ID" = o."employee_ID"
+            ORDER BY
+                o."ID"
+            """)
+        return self.cur.fetchall()
+
     def get_attendance(self, start_date: str, end_date: str) -> int:
         self.cur.execute("SELECT get_expenses(%s, %s)", (start_date, end_date))
         return self.cur.fetchall()
@@ -95,13 +184,32 @@ class Model:
         self.cur.execute("SELECT get_income(%s, %s)", (start_date, end_date))
         return self.cur.fetchall()
 
+    def get_client_id_by_phone(self, phone_number: str) -> int:
+        self.cur.execute(
+            'SELECT cl."ID" FROM "Clients" cl WHERE cl."phone_number" = %s',
+            (phone_number,)
+        )
+        res = self.cur.fetchall()
+        if len(res):
+            return res[0][0]
+        return -1
+
+    # TODO: добавить увеличение стоимости заказа
+    def add_to_order_cost(self, item_id: int):
+        pass
+
+    # TODO: добавить получение состава заказа
+    def get_menu_items_from_order(self, order_id: int) -> list[Any]:
+        pass
+
     def close(self):
         self.cur.close()
         self.conn.close()
 
 
 if __name__ == "__main__":
+    res = Model().get_client_id_by_phone("2asl;oikdjihfa;sldkfjal;skj");
+
     mod = Model()
-    mod.insert_client("123asd41ssdfs", "sdf1")
-    
+
     print(mod.get_clients()[-1])
